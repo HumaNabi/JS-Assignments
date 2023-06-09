6c8b55d386d950bf858226000716122909c82d67
HumaNabi <59052759+HumaNabi@users.noreply.github.com>
2023-06-09 23:58:55 +0500

    Add files via upload

---
+++ JavaScipt Assignments/app.js
@@ -0,0 +1,389 @@
+// **************1-Alerts********************
+
+//1-alert("Welcome to javascript word!");
+ 
+ //2- alert(<h1> "JavaScript Alert"</h1>);
+ 
+ //alert("Please!Enter a valid password");
+ 
+ //3-alert("Welcome to JS land....\n happy coding");
+//  4- alert("Welcome to Js Land");
+//   alert("Happy Coding!");
+
+//   5- alert("Hello .... I can run JS through my web browsers's  console ");
+  
+
+
+  //.............2-VARIABLES FOR STRINGS ..........
+  //  var username;
+  //  var myName="Huma Nabi";
+
+  //  var message;
+  //  message="Hello World!";
+  //  alert(message);
+
+  //  var name = prompt(" Enter student name :")
+  //  var age = prompt(" Enter student age: ")
+  //  var course= prompt(" Enter student course: ")
+
+  //  alert( name);
+  //  alert(age);
+  //  alert(course);
+
+//    var word= "Pizza";
+//     var alertmessage="";
+//    for(var i= word.length; i>=1; i--){
+//     alertmessage+= word.slice(0,i) + "\n";
+//    }
+
+// alert(alertmessage);
+// var  email=" example@example.com";
+// var message= "your email address is : "+email;
+// alert( message);
+
+// var book = "A smarter way to learn JavaScript";
+//  var message=" I am trying to learn from the Book." +book;
+//  alert(message);
+
+
+   
+  // document.Write("Yah! I can write HTML content through Javascript");
+
+  //  var symbol= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+  //  alert(symbol);
+  //  document.write(symbol);
+
+   //----------------------3-VARIABLES FOR NUMBERS----------
+    // var age = 22;
+    // alert("I am " +age);
+
+    // var visitCount = localStorage.getItem("visitCount");
+    // if (visitCount) {
+    
+    //   visitCount = parseInt(visitCount) + 1;
+    // } else {
+     
+    //   visitCount = 1;
+    // }
+    // localStorage.setItem("visitCount", visitCount);
+    
+    // var message = "You have visited this site " + visitCount + " times";
+    // document.write(message);
+    
+//     var birthYear=2000;
+//     alert("My birthday year is" + birthYear);
+//     document.write("\n Data type of my declared variable is number");
+
+// var visitorsName= "John";
+// var Producttitle= " T-shrt";
+// var quantity= 5;
+
+// var message= visitorsName + "ordered"+ quantity+  "" +Producttitle+ "on XYZ Xlothing store"
+// document.write(message);
+
+//---------------------4-VARIABLE NAMES: LEGAL & ILLEGAL------------------
+// var name="Huma", course="web development", timings="4:00-6:00";
+//  var name;
+//  var productTitle;
+//  var age;
+//  var _rate;
+//  var myVariable;
+
+//  var 123name, let, function, product-qunatity, my myVariable;
+ 
+// --------------5-MATH EXPRESSIONS----------
+// var num1= 5;
+// var num2 = 3;
+// var sum= num1+num2;
+// alert("Sum of 3 and  5 is " +sum);
+
+//var num1= 5;
+// var num2 = 3;
+// var sub= num1-num2;
+// alert("Subtraction of 3 and  5 is " +sub);
+
+//var num1= 5;
+// var num2 = 3;
+// var divide= num1+num2;
+// alert("Division of 3 and  5 is " +divide;
+
+
+
+// var num1= 5;
+// var num2 = 3;
+// var multiply= num1*num2;
+// var ans= "Multiplication of " +num1+ "and" +num2+ "is" +multiply
+// alert(ans);
+
+// var num1= 5;
+// var num2 = 3;
+// var mod= num1%num2;
+// alert("Modulus of 3 and  5 is " +mod);
+
+// var myVariable;
+
+// document.write("Value after variable declaration is: ??" +myVariable);
+// myVariable=5;
+// document.write(" <br> Initial value:"+myVariable)
+// myVariable++;
+//  document.write(" <br> Value after increment is :" +myVariable)
+// myVariable+=7;
+// document.write("<br>value after addition is : "+myVariable)
+// myVariable--;
+// document.write("<br>Value after decrement is :" +myVariable)
+// myVariable /3;
+// document.write("<br>The reaminder is :" +myVariable)
+
+// //movie ticket
+// var movieCost= 600 ;
+// var buyingTicket=5;
+// var totalCost= 600*5;
+// alert("Total cost to buy 5 tickets to a movoie is " +totalCost)
+
+// //multiplication table
+
+//  var table=4;
+//  var Table;
+
+
+// for(var i=1; i >=10; i++)
+// {
+//   console.log(Table= table*i);
+// }
+// //alert(Table);
+
+// // The temperature Coverter
+// celsius = 28
+// fahrenheit = (celsius * 9/5) + 32
+// result1 = "celsius is in " +fahrenheit
+// document.write(result1);
+ 
+
+ //Marksheet
+ //<h1 id='marksheet'> Marks Sheet</h1>
+//   var totalmarks= 980;
+//   var MakrsObtained= 804;
+//   var percentage=(MakrsObtained/totalmarks)*100;
+// document.write("Total marks :"    +totalmarks)
+// document.write("Marks Otained:"     +MakrsObtained)
+//   document.write(percentage);
+//   //Math.percentage(MakrsObtained/totalmarks)
+//  
+//  var usd_to_pkr = 104.80
+// var sar_to_pkr = 28
+// var result;
+// var totalPkr;
+// totalPkr = (10 * usd_to_pkr) + (25 * sar_to_pkr)
+
+// document.write("Total currency in Pkr :" +totalPkr)
+
+// // Age calculator
+// var currenyYear= 2023;
+// var birthyear= 2000;
+// var exactage= currenyYear- birthyear;
+// var possibleage= exactage +2;
+// document.write(exactage);
+// document.write(possibleage);
+
+// //-----------------------6-MATH EXPRESSIONS----------------------------
+
+// //greetings
+// var name= prompt("Enter your name")
+// alert("Hello!" +name+ "nice to meet you");
+
+// // subject marks
+// var subject1 = prompt("Enter the name of subject 1:");
+// var subject2 = prompt("Enter the name of subject 2:");
+// var subject3 = prompt("Enter the name of subject 3:");
+
+// var totalMarks = 100;
+// var obtainedMarks1 = parseInt(prompt("Enter obtained marks for subject 1:"));
+// var obtainedMarks2 = parseInt(prompt("Enter obtained marks for subject 2:"));
+// var obtainedMarks3 = parseInt(prompt("Enter obtained marks for subject 3:"));
+
+// var totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+// var percentage = (totalObtainedMarks / (3 * totalMarks)) * 100;
+
+// var tableContent =
+//   "<table>" +
+//   "<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>" +
+//   "<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks1 + "</td></tr>" +
+//   "<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks2 + "</td></tr>" +
+//   "<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks3 + "</td></tr>" +
+//   "<tr><th>Total</th><th>" + (3 * totalMarks) + "</th><th>" + totalObtainedMarks + "</th></tr>" +
+//   "<tr><th>Percentage</th><th colspan='2'>" + percentage.toFixed(2) + "%</th></tr>" +
+//   "</table>";
+
+// document.write(tableContent);
+
+//----------------------------7USER INPUT & CONDITIONAL TATEMENT--------------
+ // City condition 
+// var city= prompt ("Enter city name:")
+//  if (city == "Karachi")
+//  {
+//   console.log("Welcome to city of lights")
+//  }
+//  else{
+//   console.log("Bye bye")
+//  }
+
+//  // Gender decision
+//  var  gender= prompt("Enter the name:")
+
+//  if(gender==male){
+//   console.log("Good Morning Sir")
+
+//  }
+//  else if (gender== female)
+//  { console.log("Good Morning Mam")
+
+//  }
+//  else{
+//   console.log("")
+//  }
+//  // road traffic signal condition
+ 
+//  if(signalColor==Red){
+//   console.log("Must Stop")
+
+//  }
+//  else if (signalColor== Yellow)
+//  { console.log("Ready to move")
+
+//  }
+//  else if (signalColor==Green){
+//   console.log("Move now")
+//  }
+//  else
+//  {
+// console.log ("")
+//  }
+//  // calculator
+//  var num1= parseint(prompt("Enter a number:"))
+//  var num2= parseint(prompt("Enter a number:"))
+//  var operations= input("Enter the operation (+, -, *, /, %): ")
+
+//  if(operations== "+")
+//  {
+//   result = num1+num2;
+//  }
+//  elseif(operations== "-")
+//  {
+//   result = num1-num2;
+//  }
+//  elseif(operations== "*")
+//  {
+//   result = num1*num2;
+//  }
+//  elseif(operations== "/")
+//  {
+//   result = num1/num2;
+//  }
+//  elseif(operations== "%")
+//  {
+//   result = num1/num2;
+//  } 
+//  else {
+//   document.write("inavlid ")
+//  }
+ 
+//-----------------8-IF…ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITIONS------
+// greater and equal no 
+var num1= parseInt(prompt("Enter first number:"))
+var num2= parseInt(prompt("Enter secnd number:"))
+if (num1>num2)
+{
+  console.log(num1 + "is larger");
+}
+elseif (num2>num1)
+{
+  console.log(num2 + "is larger");
+}
+else{
+  console.log("Both are equal");
+}
+
+//positive,negative and zero no
+var number = parseFloatt(prompt("Enter a number: "))
+
+
+if (number > 0){
+    console.log("The number is positive.");}
+elif (number < 0){
+  console.log("The number is negative.");}
+else 
+{
+    console.log("The number is zero.");}
+
+    //-------------------------ARRAYS ---------------------
+    //JS literal notation
+    var studentName = []
+    //JS object notation
+    var StudentsNam= new Array();
+
+    //Declare and initialize a strings arra
+     var Subjectnames=["english","computer Science","Math"];
+     console.log(Subjectnames);
+
+     //. Declare and initialize a numbers array.
+     var numberarray=[1,2,3,]
+     console.log(numberarray);
+
+     //Declare and initialize a boolean array.
+     var bool=[true, false];
+     console.log(bool);
+
+     //. Declare and initialize a mixed array.
+     var mixexedarray=[true, "WEB DEVELOPMENRT",2,'c']
+     //
+     var qualification   =["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+      console.log([qualification]);
+
+      //Write a program to store student scores in an array & sort the array in ascending order using Array’s sort method.
+       var studentsScore=[320,230,480,120]
+       scores.sort(function(a, b) {
+        return  a - b;
+      });
+      y
+      console.log(scores);
+
+      //------------------------ARRAYS AND LOOP------------------------
+      //Declare and initialize an empty multidimensional array.(Array of arrays)
+     var multiarray=[];
+     multiarray[0]=[];
+     multiarray[1]=[];
+     multiarray[2]=[];
+
+     //Write a program to print numeric counting from 1 to 10
+      for (var i=0;i<=10;i++)
+      {
+        console.log(i);
+      }
+
+      //Write a program to print items of the following array using for loop:
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+for( var i=0; i< fruits.length; i++){
+  console.log([i]);
+}
+
+
+//------------------------------STRING METHODS--------------------
+
+//Write a program that takes two user inputs for first and last name using prompt and merge them in a new variable titled fullName. Greet the user using his full name.
+  var firstname=prompt("Enter first name:");
+  var lasttname=prompt("Enter last name:")
+  var fullName= firstname + lasttname;
+  document.write(fullName);
+
+  //Write a program to find the index of letter “n” in the word“Pakistani” and display the result in your browser 
+
+  var mystring= "Pakistani";
+  var word= mystring.indexOf('n');
+  document.write(word);
+
+  //-------------------------------------MATH METHODS-----------------------------
+  //Write a program that displays the absolute value of a 
+var num=-4;
+
+var value=Math.abs(num);
+document.write(value);
